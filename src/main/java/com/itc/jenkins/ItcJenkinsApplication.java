4dd8e0f492e60a26815c372e18df0a9e9a2346ff
Sumit.Patnaik <sumit.patnaik@itcinfotech.com>
2024-07-25 06:13:45 +0000

    this is inital

---
+++ src/main/java/com/itc/jenkins/ItcJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.itc.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itc/jenkins/ItcJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
